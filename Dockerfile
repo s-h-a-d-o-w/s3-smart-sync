e60d3501a16273933e9443a173c956a51a5f66d0
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2024-11-28 19:07:30 +0100

    Attempt to reduce docker resource usage

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,10 @@
 FROM node:20-alpine AS base
 
-ENV PNPM_HOME="/pnpm"
-ENV PATH="$PNPM_HOME:$PATH"
 ENV NODE_ENV production
 ENV PORT 80
 
-RUN corepack enable
-WORKDIR /app
 # Building presumably already happened
 COPY . .
 
-RUN pnpm install --frozen-lockfile
-
 EXPOSE 80
-CMD [ "pnpm", "start" ]
+CMD [ "node", "server.js" ]
--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,14 @@ set -e
 export CAPROVER_APP=s3-smart-sync
 export CAPROVER_TAR_FILE=./caprover_deployment.tar
 
+pnpm esbuild ./src/server.ts --bundle --platform=node --target=node20 --external:*.node --outfile=server.js
+
 echo "Creating archive out of repo and build artifacts..."
-tar -cf ./caprover_deployment.tar --exclude=node_modules/* .
+tar -cf ./caprover_deployment.tar server.js
 
 echo "Deploying to machine 01..."
 export CAPROVER_URL=$CAPROVER_MACHINE_01
 npx caprover deploy > /dev/null
 
+rm server.js
 rm caprover_deployment.tar
