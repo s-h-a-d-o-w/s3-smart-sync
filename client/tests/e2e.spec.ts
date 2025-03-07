ee117a30347a48a532824e70401685c5763549f7
Andreas Opferkuch <andreas.opferkuch@gmail.com>
2025-03-07 17:36:18 +0100

    Extract test utilities

--- client/tests/e2e.spec.ts
+++ client/tests/e2e.spec.ts
@@ -1,25 +1,27 @@
-import {
-  DeleteObjectsCommand,
-  GetObjectCommand,
-  ListObjectsV2Command,
-  PutObjectCommand,
-  S3Client,
-} from "@aws-sdk/client-s3";
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { fileExists } from "@s3-smart-sync/shared/fileExists.js";
-import { logger } from "@s3-smart-sync/shared/logger.js";
-import { ChildProcess, spawn } from "node:child_process";
-import { mkdir, readFile, rm, stat, writeFile } from "node:fs/promises";
-import path, { join } from "node:path";
+import { mkdir, readFile, rm, stat } from "node:fs/promises";
+import { join } from "node:path";
 import {
   ACCESS_KEY,
   AWS_REGION,
   S3_BUCKET,
   SECRET_KEY,
 } from "../src/consts.js";
-import { SNSMessage } from "aws-lambda";
 import { IGNORE_CLEANUP_DURATION } from "../src/fileWatcher.js";
-
-const SERVER_URL = process.env["WEBSOCKET_URL"]!.replace("ws", "http");
+import {
+  cleanupLocalDirectories,
+  cleanupS3,
+  createClientDirectories,
+  createFile,
+  pause,
+  sendSnsMessage,
+  startClients,
+  startServer,
+  stopClients,
+  stopServer,
+  waitUntil,
+} from "./utilities.js";
 
 const TEST_FILES = {
   "test1.txt": "Hello World",
@@ -39,136 +41,6 @@ const s3Client = new S3Client({
     secretAccessKey: SECRET_KEY,
   },
 });
-
-// Limited to 1000 objects!
-async function cleanupS3() {
-  try {
-    const { Contents } = await s3Client.send(
-      new ListObjectsV2Command({
-        Bucket: S3_BUCKET,
-      }),
-    );
-
-    if (!Contents?.length) return;
-
-    await s3Client.send(
-      new DeleteObjectsCommand({
-        Bucket: S3_BUCKET,
-        Delete: {
-          Objects: Contents.map(({ Key }) => ({ Key })),
-        },
-      }),
-    );
-  } catch (error) {
-    logger.error(`Failed to cleanup S3 bucket: ${error}`);
-  }
-}
-
-async function cleanupLocalDirs(fully?: boolean) {
-  await rm(CLIENT_1_DIR, { recursive: true, force: true });
-  await rm(CLIENT_2_DIR, { recursive: true, force: true });
-
-  if (!fully) {
-    await mkdir(CLIENT_1_DIR, { recursive: true });
-    await mkdir(CLIENT_2_DIR, { recursive: true });
-  }
-}
-
-async function createFile(baseDir: string, key: string, content: string) {
-  await writeFile(join(baseDir, key), content);
-
-  await waitUntil(async () => {
-    const { Body } = await s3Client.send(
-      new GetObjectCommand({
-        Bucket: S3_BUCKET,
-        Key: key,
-      }),
-    );
-
-    const actualContent = await Body?.transformToString();
-    return actualContent === content;
-  });
-
-  const snsMessage: SNSMessage = {
-    Type: "Notification",
-    MessageId: "dummy",
-    MessageAttributes: {},
-    TopicArn: "dummy",
-    Message: JSON.stringify({
-      Records: [
-        {
-          eventName: "ObjectCreated:Put",
-          s3: {
-            bucket: { name: S3_BUCKET },
-            object: { key: key },
-          },
-        },
-      ],
-    }),
-    Timestamp: new Date().toISOString(),
-    SignatureVersion: "1",
-    Signature: "test-signature",
-    SigningCertUrl: "test-cert-url",
-    UnsubscribeUrl: "test-unsub-url",
-  };
-
-  await fetch(SERVER_URL + "/sns", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(snsMessage),
-  });
-}
-
-async function sendSnsMessage(key: string, operation: "put" | "delete") {
-  const message = {
-    Type: "Notification",
-    MessageId: "dummy",
-    MessageAttributes: {},
-    TopicArn: "dummy",
-    Message: JSON.stringify({
-      Records: [
-        {
-          eventName:
-            operation === "put" ? "ObjectCreated:Put" : "ObjectRemoved:Delete",
-          s3: {
-            bucket: { name: S3_BUCKET },
-            object: { key },
-          },
-        },
-      ],
-    }),
-    Timestamp: new Date().toISOString(),
-    SignatureVersion: "1",
-    Signature: "test-signature",
-    SigningCertUrl: "test-cert-url",
-    UnsubscribeUrl: "test-unsub-url",
-  };
-
-  await fetch(SERVER_URL + "/sns", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(message),
-  });
-}
-
-function killProcess(proc: ChildProcess | undefined): Promise<void> {
-  return new Promise((resolve) => {
-    if (!proc) {
-      resolve();
-      return;
-    }
-
-    proc.once("exit", () => {
-      resolve();
-    });
-
-    proc.kill("SIGINT");
-  });
-}
-
-function pause(ms: number) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
 
 async function verifyFiles(clientDir: string) {
   for (const [key, expectedContent] of Object.entries(TEST_FILES)) {
@@ -193,50 +65,10 @@ async function verifyFiles(clientDir: string) {
   }
 }
 
-async function waitForServer() {
-  await waitUntil(async () => {
-    const response = await fetch(SERVER_URL);
-    return response.ok && (await response.text()) === "Running.";
-  });
-}
-
-async function waitUntil(
-  fn: () => unknown,
-  {
-    interval = 200,
-    timeout = 3000,
-  }: { interval?: number; timeout?: number } = {},
-) {
-  const startTime = Date.now();
-
-  while (timeout === 0 || Date.now() - startTime < timeout) {
-    try {
-      // only exceptions or returning false will result in continuation
-      if ((await fn()) === false) {
-        await pause(interval);
-        continue;
-      }
-
-      return;
-    } catch (_) {
-      // continue
-    }
-
-    await pause(interval);
-  }
-
-  throw new Error("Timeout waiting for condition");
-}
-
-let serverProcess: ChildProcess | undefined;
-let client1Process: ChildProcess | undefined;
-let client2Process: ChildProcess | undefined;
-
 describe("E2E Tests", () => {
   beforeAll(async () => {
     // test file syncing while we start everything necessary for all tests
-    await mkdir(CLIENT_1_DIR, { recursive: true });
-    await mkdir(CLIENT_2_DIR, { recursive: true });
+    await createClientDirectories([1, 2]);
 
     for (const [key, content] of Object.entries(TEST_FILES)) {
       await s3Client.send(
@@ -248,29 +80,8 @@ describe("E2E Tests", () => {
       );
     }
 
-    serverProcess = spawn(
-      "node",
-      [path.join(__dirname, "../../server/server.js")],
-      { env: process.env, stdio: "inherit" },
-    );
-    await waitForServer();
-
-    client1Process = spawn(
-      "node",
-      [path.join(__dirname, "../dist/index.cjs"), "cli"],
-      {
-        // stdio: "inherit",
-        env: { ...process.env, LOCAL_DIR: CLIENT_1_DIR },
-      },
-    );
-    client2Process = spawn(
-      "node",
-      [path.join(__dirname, "../dist/index.cjs"), "cli"],
-      {
-        // stdio: "inherit",
-        env: { ...process.env, LOCAL_DIR: CLIENT_2_DIR },
-      },
-    );
+    await startServer();
+    startClients([1, 2]);
 
     await waitUntil(async () => {
       await verifyFiles(CLIENT_1_DIR);
@@ -280,21 +91,18 @@ describe("E2E Tests", () => {
 
   afterAll(async () => {
     await cleanupS3();
-    await cleanupLocalDirs(true);
-    await Promise.all([
-      killProcess(client1Process),
-      killProcess(client2Process),
-      killProcess(serverProcess),
-    ]);
+    await cleanupLocalDirectories(true);
+    await stopClients([1, 2]);
+    await stopServer();
   });
 
   beforeEach(async () => {
     await cleanupS3();
-    await cleanupLocalDirs();
+    await cleanupLocalDirectories();
   });
 
   it("should sync file changes between clients", async () => {
-    await createFile(CLIENT_1_DIR, "new-file.txt", "New content");
+    await createFile(1, "new-file.txt", "New content");
     await waitUntil(async () =>
       expect(await readFile(join(CLIENT_2_DIR, "new-file.txt"), "utf-8")).toBe(
         "New content",
@@ -303,7 +111,7 @@ describe("E2E Tests", () => {
 
     await pause(IGNORE_CLEANUP_DURATION + 10);
 
-    await createFile(CLIENT_2_DIR, "new-file.txt", "Changed content");
+    await createFile(2, "new-file.txt", "Changed content");
     await waitUntil(async () =>
       expect(await readFile(join(CLIENT_1_DIR, "new-file.txt"), "utf-8")).toBe(
         "Changed content",
@@ -312,7 +120,7 @@ describe("E2E Tests", () => {
   });
 
   it("should handle replacing a file with an empty directory", async () => {
-    await createFile(CLIENT_1_DIR, "file-then-directory", "starts as a file");
+    await createFile(1, "file-then-directory", "starts as a file");
     await waitUntil(async () =>
       expect(await fileExists(join(CLIENT_2_DIR, "file-then-directory"))).toBe(
         true,
@@ -344,7 +152,7 @@ describe("E2E Tests", () => {
     await sendSnsMessage("directory-then-file/", "delete");
     await pause(IGNORE_CLEANUP_DURATION + 10);
 
-    await createFile(CLIENT_1_DIR, "directory-then-file", "now it's a file");
+    await createFile(1, "directory-then-file", "now it's a file");
     await waitUntil(async () => {
       expect(
         await readFile(join(CLIENT_2_DIR, "directory-then-file"), "utf-8"),
---
+++ client/tests/utilities.ts
@@ -0,0 +1,211 @@
+import { ChildProcess } from "child_process";
+
+import { spawn } from "child_process";
+import { mkdir, rm, writeFile } from "fs/promises";
+import path, { join } from "path";
+import {
+  DeleteObjectsCommand,
+  GetObjectCommand,
+  ListObjectsV2Command,
+  S3Client,
+} from "@aws-sdk/client-s3";
+import {
+  ACCESS_KEY,
+  AWS_REGION,
+  S3_BUCKET,
+  SECRET_KEY,
+} from "../src/consts.js";
+import { logger } from "@s3-smart-sync/shared/logger.js";
+
+const SERVER_URL = process.env["WEBSOCKET_URL"]!.replace("ws", "http");
+
+const clients: Record<number, ChildProcess> = {};
+let serverProcess: ChildProcess | undefined;
+
+const s3Client = new S3Client({
+  region: AWS_REGION,
+  credentials: {
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_KEY,
+  },
+});
+
+// Limited to 1000 objects!
+export async function cleanupS3() {
+  try {
+    const { Contents } = await s3Client.send(
+      new ListObjectsV2Command({
+        Bucket: S3_BUCKET,
+      }),
+    );
+
+    if (!Contents?.length) return;
+
+    await s3Client.send(
+      new DeleteObjectsCommand({
+        Bucket: S3_BUCKET,
+        Delete: {
+          Objects: Contents.map(({ Key }) => ({ Key })),
+        },
+      }),
+    );
+  } catch (error) {
+    logger.error(`Failed to cleanup S3 bucket: ${error}`);
+  }
+}
+
+export async function createClientDirectories(ids: number[]) {
+  for (const id of ids) {
+    const clientDirectory = join(__dirname, `test-client-${id}`);
+    await mkdir(clientDirectory, { recursive: true });
+  }
+}
+
+export function startClients(ids: number[], debugId?: number) {
+  for (const id of ids) {
+    const clientDirectory = join(__dirname, `test-client-${id}`);
+    const clientProcess = spawn(
+      "node",
+      [path.join(__dirname, "../dist/index.cjs"), "cli"],
+      {
+        stdio: id === debugId ? "inherit" : undefined,
+        env: { ...process.env, LOCAL_DIR: clientDirectory },
+      },
+    );
+    clients[id] = clientProcess;
+  }
+}
+
+function killProcess(proc: ChildProcess | undefined): Promise<void> {
+  return new Promise((resolve) => {
+    if (!proc) {
+      resolve();
+      return;
+    }
+
+    proc.once("exit", () => {
+      resolve();
+    });
+
+    proc.kill("SIGINT");
+  });
+}
+
+export async function stopClients(ids?: number[]) {
+  await Promise.all(
+    (ids || Object.keys(clients).map(Number)).map(async (id) => {
+      await killProcess(clients[id]);
+    }),
+  );
+}
+
+export async function cleanupLocalDirectories(fully?: boolean) {
+  for (const id of Object.keys(clients)) {
+    const clientDirectory = join(__dirname, `test-client-${id}`);
+    await rm(clientDirectory, { recursive: true, force: true });
+
+    if (!fully) {
+      await mkdir(clientDirectory, { recursive: true });
+    }
+  }
+}
+
+export async function createFile(id: number, key: string, content: string) {
+  const clientDirectory = join(__dirname, `test-client-${id}`);
+  await writeFile(join(clientDirectory, key), content);
+
+  await waitUntil(async () => {
+    const { Body } = await s3Client.send(
+      new GetObjectCommand({
+        Bucket: S3_BUCKET,
+        Key: key,
+      }),
+    );
+
+    const actualContent = await Body?.transformToString();
+    return actualContent === content;
+  });
+
+  await sendSnsMessage(key, "put");
+}
+
+export async function sendSnsMessage(key: string, operation: "put" | "delete") {
+  const message = {
+    Type: "Notification",
+    MessageId: "dummy",
+    MessageAttributes: {},
+    TopicArn: "dummy",
+    Message: JSON.stringify({
+      Records: [
+        {
+          eventName:
+            operation === "put" ? "ObjectCreated:Put" : "ObjectRemoved:Delete",
+          s3: {
+            bucket: { name: S3_BUCKET },
+            object: { key },
+          },
+        },
+      ],
+    }),
+    Timestamp: new Date().toISOString(),
+    SignatureVersion: "1",
+    Signature: "test-signature",
+    SigningCertUrl: "test-cert-url",
+    UnsubscribeUrl: "test-unsub-url",
+  };
+
+  await fetch(SERVER_URL + "/sns", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(message),
+  });
+}
+
+export async function startServer() {
+  serverProcess = spawn(
+    "node",
+    [path.join(__dirname, "../../server/server.js")],
+    { env: process.env, stdio: "inherit" },
+  );
+
+  await waitUntil(async () => {
+    const response = await fetch(SERVER_URL);
+    return response.ok && (await response.text()) === "Running.";
+  });
+}
+
+export async function stopServer() {
+  await killProcess(serverProcess);
+}
+
+export async function waitUntil(
+  fn: () => unknown,
+  {
+    interval = 200,
+    timeout = 3000,
+  }: { interval?: number; timeout?: number } = {},
+) {
+  const startTime = Date.now();
+
+  while (timeout === 0 || Date.now() - startTime < timeout) {
+    try {
+      // only exceptions or returning false will result in continuation
+      if ((await fn()) === false) {
+        await pause(interval);
+        continue;
+      }
+
+      return;
+    } catch (_) {
+      // continue
+    }
+
+    await pause(interval);
+  }
+
+  throw new Error("Timeout waiting for condition");
+}
+
+export function pause(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
